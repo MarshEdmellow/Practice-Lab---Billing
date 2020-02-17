c790cbf93514e2bc7778fef94ab14edf9952f9ec
Edmar Bucsit <edmar.bucsitjr.iics@ust.edu.ph>
2020-02-17 21:15:15 +0800

    Second Commit - Billing

--- js/index.js
+++ js/index.js
@@ -1,12 +1,32 @@
+/*Add the JavaScript here for the function billingFunction(). 
+It is responsible for setting and clearing the fields in Billing Information */
 function billingFunction(){
-		if (document.getElementById("same").checked){
-                var shippingName =  document.getElementById("shippingName").value;
-                var shippingZip = document.getElementById("shippingZip").value;
-                document.getElementById('billingName').value = shippingName;
-                document.getElementById('billingZip').value = shippingZip;
-		}
-		else{ 
-			document.getElementById('billingName').value = "";
-			document.getElementById('billingZip').value = "";
-		}
+    var name = document.getElementById("shippingName");
+    var zip = document.getElementById("shippingZip");
+    var billName = document.getElementById("billingName");
+    var billZip = document.getElementById("billingZip");
+    var same = document.getElementById("same");
+    
+    if(same.checked ){
+        billName.value = name.value;
+        billZip.value = zip.value;
+    }
+    
 }
+function Verify(){
+    var name = document.getElementById("shippingName");
+    var zip = document.getElementById("shippingZip");
+    var billName = document.getElementById("billingName");
+    var billZip = document.getElementById("billingZip");
+    var same = document.getElementById("same");
+
+
+    if(name.value === billName.value && zip.value === billZip.value){
+        alert("Your entries are match!");
+    }
+    else{
+        alert("Shipping and Billing information are not the same!");
+    }
+
+}
+Verify();
